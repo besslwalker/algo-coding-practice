c89585802924c9d029c04c564e75e6ecd38e6720
Bess L. Walker <bess.l.walker@gmail.com>
2017-01-16 21:46:21 -0600

    Now with tests
    Apparently late 2011 Bess was much better at incorporating tests than
    early 2017 Bess. I have learned from her.

--- 1.0 Insertion Sort/insertionSort.js
+++ 1.0 Insertion Sort/insertionSort.js
@@ -1,7 +1,49 @@
-// performs in-place sorting
-// we could return a sorted copy by performing the
-// same in-place sort on a slice()'d copy (array.slice())
-// after making sure array exists
+// Insertion Sort (Integers)
+// Bess L. Walker
+// 1-16-17
+// Practicing algorithms & coding using The Algorithm Design Manual
+
+(function test() {
+	
+	var permut_0 = undefined;
+	
+	console.log(permut_0);
+	insertionSort(permut_0);
+	console.log(permut_0);
+	
+	console.log();
+	
+	var permut_1 = [];
+	
+	console.log(permut_1);
+	insertionSort(permut_1);
+	console.log(permut_1);
+	
+	console.log();
+	
+	var permut_2 = [0, 1, 2, 3, 4, 5];
+	
+	console.log(permut_2);
+	insertionSort(permut_2);
+	console.log(permut_2);
+	
+	console.log();
+	
+	var permut_3 = [5, 4, 3, 2, 1, 0];
+	
+	console.log(permut_3);
+	insertionSort(permut_3);
+	console.log(permut_3);
+	
+	console.log();
+	
+	var permut_4 = [2, 11, 14, 1, 0, 2];
+	
+	console.log(permut_4);
+	insertionSort(permut_4);
+	console.log(permut_4);
+})();
+
 function insertionSort(array) {
 	if (!array || array.length <= 1) {return array;}
 	
@@ -15,10 +57,8 @@ function insertionSort(array) {
 				array[j] = array[i];
 				array[i] = temp;
 			}
-			else {
-				// found the right place, stop
-				break;
-			}
+			// can't short-circuit on array[i] >= array[j] here
+			// if the array supports duplicates
 		}
 	}
 	
